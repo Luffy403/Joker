cae764065a18dcd3a5e2182d87062ff6d0ca5ca8
Luffy403 <gdhjyt68vhbe85.h@gmail.com>
2024-12-14 17:45:47 +0500

    Add files via upload

---
+++ dz_3/Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите первое число ежи: ");
+            int firstnumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число ежи: ");
+            int secondnumber = int.Parse(Console.ReadLine());
+            int sum = firstnumber + secondnumber;
+            Console.WriteLine("Их сумма: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ dz_3/Game/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random random403 = new Random();
+            int secretNumber = random403.Next(1, 11);
+            int number;
+            int popitka = 0;
+
+            Console.WriteLine("Я загадал число от 1 до 10. Попробуйте угадать его!");
+            do
+            {
+                Console.Write("Введите ваше предположение: ");
+
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    popitka++;
+                    if (number < secretNumber)
+                        Console.WriteLine("Загаданное число больше.");
+                    else if (number > secretNumber)
+                        Console.WriteLine("Загаданное число меньше.");
+                }
+                else
+                {
+                    Console.WriteLine("Введите корректное целое число.");
+                }
+            } 
+            while (number != secretNumber);
+            
+            Console.WriteLine($"Поздравляю! Вы угадали число {secretNumber} за {popitka} попыток.");
+            Console.ReadKey();
+        }
+    }
+}
