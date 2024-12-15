c6997dc0cc532fadd79a497a788939d402c75bbc
Luffy403 <gdhjyt68vhbe85.h@gmail.com>
2024-12-15 17:26:06 +0500

    Add files via upload

---
+++ dz_4/Practica_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] massiv1 = { 1, 2, 3, 403 };
+            int[] massiv2 = { 7, 98, 2, 4 };
+            int[] megamassiv = new int[massiv1.Length + massiv2.Length];
+            for (int i = 0; i < massiv1.Length; i++ )
+            {
+                megamassiv[i] = massiv1[i];
+            }
+            for (int i = 0; i < massiv2.Length; i++)
+            {
+                megamassiv[massiv1.Length + i] =  massiv2[i];
+            }
+            Console.WriteLine("Каждый элемент массива: ");
+            foreach (int number in megamassiv)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ dz_5/Practica_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 3, 2, 3, 4, 3, 5, 2, 1, 2, 2};
+        int mostFrequent = Mostnumber(numbers);
+        Console.WriteLine("Часто встречающийся элемент: " + mostFrequent);
+        Console.ReadKey();
+    }
+
+    static int Mostnumber(int[] array)
+    {
+        int mostFrequent = array[0];
+        int maxCount = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            int count = 0;
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (array[j] == array[i])
+                {
+                    count++;
+                }
+            }
+
+            if (count > maxCount)
+            {
+                maxCount = count;
+                mostFrequent = array[i];
+            }
+        }
+
+        return mostFrequent;
+    }
+}
